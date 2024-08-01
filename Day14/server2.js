c1f6f15b1eb2111564637cf36c797ef91b7ecf7c
ChamarthiNagalakshmi <161118303+ChamarthiNagalakshmi@users.noreply.github.com>
2024-08-01 02:11:25 -0700

    Add files via upload

---
+++ Day14/server2.js
@@ -0,0 +1,9 @@
+var url = require('url');
+var adr = 'http://www.facebook.com?year=2024&month=August';
+//parse the address:
+var q = url.parse(adr,true);
+console.log(q.host);
+console.log(q.pathname);
+console.log(q.search);
+var qdata = q.query;
+console.log(qdata.month);
